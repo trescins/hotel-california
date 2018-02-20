fee227a6313110d462d893aaa33ef88cafecbd2c
Front-end Together <frontendwspolnie@gmail.com>
2018-02-20 19:57:55 +0100

    featured/menu_jQueryShuffleEffect

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+$(function(){
+	
+	// container is the DOM element;
+	
+	var container = $("#container");
+	
+	// Shuffle the contents of container
+	container.shuffleLetters();
+
+	setTimeout(function(){	},4000);
+	
+});
